39a6cfd93e2832736b8b9a2a46c21a131bd58453
sanathkumargadipe <164140297+sanathkumargadipe@users.noreply.github.com>
2026-03-17 12:04:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -137,10 +137,7 @@ st.markdown("---")
 # IMAGE SECTION (UNCHANGED)
 # -----------------------------------------------------
 
-st.image(
-    "https://www.evlo.co.uk/wp-content/uploads/2025/06/aicreditrisk.jpg.webp",
-    use_column_width=True
-)
+st.image("https://www.evlo.co.uk/wp-content/uploads/2025/06/aicreditrisk.jpg.webp", use_column_width=True)
 
 st.markdown("---")
 
